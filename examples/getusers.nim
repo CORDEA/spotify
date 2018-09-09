f69a5ecde871eb2e21edc0a58c245c43c08b6c92
Yoshihiro Tanaka <contact@cordea.jp>
2018-09-09 17:16:21 +0900

    add example

---
+++ examples/getusers.nim
@@ -0,0 +1,35 @@
+# Copyright 2018 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+  # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2018-09-09
+
+import ospaths
+import httpclient
+import .. / src / users
+import .. / src / spotifyclient
+
+let
+  token = newHttpClient().authorizationCodeGrant(
+    getEnv("SPOTIFY_ID"),
+    getEnv("SPOTIFY_SECRET"),
+    @[]
+  )
+  client = newSpotifyClient(token.accessToken, token.refreshToken, token.expiresIn)
+  user = client.getCurrentUser()
+  wizzler = client.getUser("wizzler")
+
+echo user.id
+echo user.displayName
+echo wizzler.id
+echo wizzler.displayName
