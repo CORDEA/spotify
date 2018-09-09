2c33f14cab476ba3e2ec5996848ca75483b4df34
Yoshihiro Tanaka <contact@cordea.jp>
2018-09-09 18:28:27 +0900

    add SimpleTrack object

---
+++ src/objects/simpletrack.nim
@@ -0,0 +1,31 @@
+# Copyright 2018 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+  # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2018-09-09
+
+import tracklink
+import externalurl
+import simpleartist
+import restrictions
+
+type
+  SimpleTrack* = ref object
+    href*, id*, name*, previewUrl*, objectType*, uri*: string
+    discNumber*, durationMs*, trackNumber*: int
+    explicit*, isPlayable*, isLocal*: bool
+    artists*: seq[SimpleArtist]
+    availableMarkets*: seq[string]
+    externalUrls*: seq[ExternalUrl]
+    restrictions*: Restrictions
+    linkedFrom*: TrackLink
