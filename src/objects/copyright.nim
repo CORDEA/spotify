e181fbbe4637720ac25e701f69ff90929347bc94
Yoshihiro Tanaka <contact@cordea.jp>
2018-09-09 18:22:13 +0900

    add Copyright object

---
+++ src/objects/copyright.nim
@@ -0,0 +1,33 @@
+# Copyright 2018 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+  # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2018-09-04
+
+import json
+
+type
+  CopyrightType* = enum
+    TypeCopyright = "C",
+    TypeSoundRecordingCopyright = "P"
+  Copyright* = ref object
+    text*: string
+    copyrightType*: CopyrightType
+
+proc unmarshal*(node: JsonNode, t: Copyright) =
+  t.text = node["text"].getStr
+  case node["type"].getStr
+  of $TypeCopyright:
+    t.copyrightType = TypeCopyright
+  of $TypeSoundRecordingCopyright:
+    t.copyrightType = TypeSoundRecordingCopyright
