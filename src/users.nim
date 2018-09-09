e087c72c44a428759964c6ee65e45801df354553
Yoshihiro Tanaka <contact@cordea.jp>
2018-09-09 16:52:22 +0900

    add proc to get current user

---
+++ src/users.nim
@@ -0,0 +1,29 @@
+# Copyright 2018 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+  # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2018-09-02
+
+import httpclient
+import spotifyclient
+import asyncdispatch
+import objects / user
+
+const
+  GetMePath = "/me"
+
+proc getCurrentUser*(client: SpotifyClient | AsyncSpotifyClient): Future[User] {.multisync.} =
+  let
+    response = await client.request(GetMePath)
+    body = await response.body
+  result = body.toUser()
