f34cc682afbf5fb79f9f31dc055722b0cdd4d457
Yoshihiro Tanaka <contact@cordea.jp>
2018-09-10 13:03:41 +0900

    add test

---
+++ test/tspotifyuri.nim
@@ -0,0 +1,23 @@
+# Copyright 2018 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+  # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2018-09-10
+
+import unittest
+import .. / src / spotifyuri
+
+suite "Spotifyuri test":
+  test "BuildPath":
+    let path = buildPath("/example", @[newQuery("a", "1"), newQuery("a", "1"), newQuery("b", "2")])
+    check(path == "/example?a=1&a=1&b=2")
