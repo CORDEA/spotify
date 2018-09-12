0a6da8c4db213b23ad60290d08357d0667390666
Yoshihiro Tanaka <contact@cordea.jp>
2018-09-12 16:15:04 +0900

    add SimplePlaylist object

---
+++ src/objects/simpleplaylist.nim
@@ -0,0 +1,49 @@
+# Copyright 2018 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+  # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2018-09-12
+
+import json
+import image
+import paging
+import tracklink
+import jsonparser
+import publicuser
+import externalurl
+
+type
+  PlaylistVisibility* = enum
+    TypePublic, TypePrivate, TypeNotRelevant
+  SimplePlaylist* = ref object
+    collaborative*: bool
+    href*, id*, name*, snapshotId*: string
+    objectType*, uri*: string
+    externalUrls*: seq[ExternalUrl]
+    images*: seq[Image]
+    owner*: PublicUser
+    public*: PlaylistVisibility
+    tracks*: TrackLink
+
+proc unmarshal*(node: JsonNode, v: var PlaylistVisibility) =
+  if node.kind == JNull:
+    v = TypeNotRelevant
+    return
+  if node.getBool:
+    v = TypePublic
+  else:
+    v = TypePrivate
+
+proc toSimplePlaylist*(json: string): Paging[SimplePlaylist] =
+  let node = parseJson json
+  unmarshal(node["playlists"], result)
