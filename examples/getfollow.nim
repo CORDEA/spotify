49e06dc4fb9d9f8fce0cd77921614fa500f33962
Yoshihiro Tanaka <contact@cordea.jp>
2018-09-15 13:48:00 +0900

    add example

---
+++ examples/getfollow.nim
@@ -0,0 +1,53 @@
+# Copyright 2018 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+  # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2018-09-15
+
+import ospaths
+import httpclient
+import .. / src / scope
+import .. / src / follow
+import .. / src / spotifyclient
+
+const
+  artists = @["74ASZWbe4lXaubB36ztrGX", "08td7MxkoHQkXnWAYD8d6Q"]
+  users = @["exampleuser01"]
+  playlist = "2v3iNvBX8Ay1Gt2uXtUKUT"
+
+let
+  token = newHttpClient().authorizationCodeGrant(
+    getEnv("SPOTIFY_ID"),
+    getEnv("SPOTIFY_SECRET"),
+    @[ScopeUserFollowRead, ScopeUserFollowModify, ScopePlaylistModifyPublic]
+  )
+  client = newSpotifyClient(token.accessToken, token.refreshToken, token.expiresIn)
+  isFollowArtist = client.isFollowArtist(artists)
+  isFollowUser = client.isFollowUser(users)
+  isFollowPlaylist = client.isFollowPlaylist("jmperezperez", playlist, @["possan", "elogain"])
+  followedArtists = client.getFollowedArtists()
+
+echo $isFollowArtist
+echo $isFollowUser
+echo $isFollowPlaylist
+
+for artist in followedArtists.items:
+  echo artist.name
+
+client.followArtist(artists)
+client.followUser(users)
+client.followPlaylist(playlist)
+
+client.unfollowArtist(artists)
+client.unfollowUser(users)
+client.unfollowPlaylist(playlist)
